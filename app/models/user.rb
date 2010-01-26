50c4ff525347f5a5456289ee9d119365c4cfc960
Ari Epstein <aepstein607@gmail.com>
2010-01-26 16:04:05 -0500

    Fix user password intialization.

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ActiveRecord::Base
   protected
 
   def initialize_password
-    reset_password unless password
+    reset_password unless crypted_password?
   end
 
   def ldap_entry=(ldap_entry)
