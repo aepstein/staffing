ec38cab714b434d47aa6371d05ec1b55cfb704b0
Ari Epstein <aepstein607@gmail.com>
2010-05-06 13:24:29 -0400

    Show renew link only for renewable positions

--- app/helpers/memberships_helper.rb
+++ app/helpers/memberships_helper.rb
@@ -1,2 +1,8 @@
 module MembershipsHelper
+  def link_to_renewal( membership )
+    link_to_if (membership.position.renewable? && membership.position.requestables.size > 0),
+      'Renew',
+      ( membership.request ? edit_request_path(membership.request) : new_membership_request_path(membership) )
+  end
 end
+
