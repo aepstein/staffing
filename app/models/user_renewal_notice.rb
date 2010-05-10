b9f0ab5cb49399721d97143053f50a578532b160
Ari Epstein <aepstein607@gmail.com>
2010-05-10 12:25:22 -0400

    Add some initializers to make renewal notices defaults more sane.

--- app/models/user_renewal_notice.rb
+++ app/models/user_renewal_notice.rb
@@ -5,7 +5,7 @@ class UserRenewalNotice < ActiveRecord::Base
 
   has_many :sendings, :as => :message, :dependent => :delete_all do
     def populate
-      return if proxy_owner.new_record?
+      return if proxy_owner.new_record? || proxy_owner.sendings_populated?
       UserRenewalNotice.transaction do
         proxy_owner.lock!
         proxy_owner.users.each { |user| create( :user => user ) }
@@ -18,6 +18,12 @@ class UserRenewalNotice < ActiveRecord::Base
   validates_date :starts_at
   validates_date :ends_at, :after => :starts_at
   validates_date :deadline, :after => :starts_at
+
+  def after_initialize
+    self.starts_at ||= Membership.current.minimum(:starts_at)
+    self.ends_at ||= Membership.current.maximum(:ends_at)
+    self.deadline ||= Date.today + 2.weeks
+  end
 
   def users
     User.no_notice_since( self.class.to_s, created_at - 1.week ).reject { |user|
