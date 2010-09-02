e5c731d3cc6f17f1d1014dc7264fa93fb2b99eae
Ari Epstein <ate2@ari-shuttle.dayadmin.cornell.edu>
2010-09-02 15:01:26 -0400

    Revert to rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -11,8 +11,6 @@ Rails::Initializer.run do |config|
   config.action_controller.session = {
     :key => '_staffing_session', :secret => Proc.new { APP_CONFIG['session_secret'] }
   }
-
-  config.action_controller.session_store = :active_record_store
 
   config.time_zone = 'Eastern Time (US & Canada)'
 
