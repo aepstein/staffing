3868c981a6672451d831da634f7a41b329b50ff4
Ari Epstein <ate2@ari-shuttle.dayadmin.cornell.edu>
2010-09-07 11:26:58 -0400

    Fix unneeded deprecation warning.

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,17 @@
-# Be sure to restart your server when you modify this file
-
-# Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+
+if Gem::VERSION >= "1.3.6"
+    module Rails
+        class GemDependency
+            def requirement
+                r = super
+                (r == Gem::Requirement.default) ? nil : r
+            end
+        end
+    end
+end
 
 Rails::Initializer.run do |config|
 
