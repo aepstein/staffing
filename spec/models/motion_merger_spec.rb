30c5380ec50bf610766ac99de535d9a3e9a57dfa
Ari Epstein <ate2@yofi.arimahome.net>
2010-10-23 09:05:58 -0400

    Add some more tests for motion merger.

--- spec/models/motion_merger_spec.rb
+++ spec/models/motion_merger_spec.rb
@@ -8,5 +8,20 @@ describe MotionMerger do
   it "should create a new instance given valid attributes" do
     @merger.id.should_not be_nil
   end
+
+  it 'should merge the merged_motion' do
+    @merger.merged_motion.merged?.should be_true
+  end
+
+  it 'should not save without a merged_motion' do
+    @merger.merged_motion = nil
+    @merger.save.should be_false
+  end
+
+  it 'should not save without a motion' do
+    @merger.motion = nil
+    @merger.save.should be_false
+  end
+
 end
 
