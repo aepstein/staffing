f2144b9401199de00cada75e3f07b5f8f303b18e
Ari Epstein <ate2@cornell.edu>
2011-01-05 13:56:56 -0500

    Try to fix issue with datetimepicker.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,10 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
   $('input.ui-date-picker').datepicker({ dateFormat: 'dd M yy' });
-});
-$(document).ready(function(){
-  $('input.ui-datetime-picker').datetimepicker({
-    dateFormat: 'dd M yy', timeFormat: 'hh:mm'
-  });
+  $('input.ui-datetime-picker').datetimepicker({ dateFormat: 'dd M yy', timeFormat: 'hh:mm'  });
 });
 
