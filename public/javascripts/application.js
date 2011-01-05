4106ab3183b07458a07a439ca8eeb72f720e839c
Ari Epstein <ate2@cornell.edu>
2011-01-05 11:42:13 -0500

    Clean up date formatting.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-  $('input.ui-date-picker').datepicker({ dateFormat: 'yy-mm-dd' });
+  $('input.ui-date-picker').datepicker({ dateFormat: 'dd M yy' });
 });
 $(document).ready(function(){
   $('input.ui-datetime-picker').datetimepicker({
-    dateFormat: 'yy-mm-dd', timeFormat: 'hh:mm'
+    dateFormat: 'dd M yy', timeFormat: 'hh:mm'
   });
 });
 
