58084b523fc8119f17dea8ba7b95d6b234c1e7a3
Ari Epstein <ate2@cornell.edu>
2011-01-05 15:32:31 -0500

    Password confirmation should be suppressed.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Staffing
   class Application < Rails::Application
     config.autoload_paths += %W(#{Rails.root}/lib)
     config.encoding = "utf-8"
-    config.filter_parameters += [:password]
+    config.filter_parameters += [ :password, :password_confirmation ]
     config.time_zone = 'Eastern Time (US & Canada)'
     config.action_mailer.default_url_options = { :host => "assembly.cornell.edu", :protocol => 'https' }
     config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui jquery-ui-timepicker-addon autocomplete-rails rails)
