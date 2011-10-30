8708211d56b97bc4695de2ea3a20da28ee6779cd
Ari Epstein <aepstein607@gmail.com>
2011-10-30 15:31:09 -0400

    Remove deprecated call from requests controller.  Fix test step.

--- features/step_definitions/request_steps.rb
+++ features/step_definitions/request_steps.rb
@@ -2,9 +2,8 @@ Given /^#{capture_model} rejects #{capture_model} with authority: #{capture_mode
   user = model who
   request = model what
   request.rejected_by_user = user
-  request.accessible = Request::REJECTABLE_ATTRIBUTES
-  request.attributes = { :rejected_by_authority_id => model(authority).id,
-    :rejection_comment => message }
+  request.assign_attributes( { :rejected_by_authority =>  model(authority),
+    :rejection_comment => message }, { :without_protection => true } )
   request.reject!
 end
 
