d29b9569f330a9643208d601fb64c2b5091a83a1
Ari Epstein <ate2@cornell.edu>
2012-01-26 11:24:46 -0500

    Remove force_ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,6 @@ Staffing::Application.configure do
   config.assets.digest = true
   config.filter_parameters += [ :password, :password_confirmation ]
   config.assets.precompile += %w( ie6.css ie7.css )
-  config.force_ssl = true
   config.middleware.use ExceptionNotifier,
     :email_prefix => "[staffing] ",
     :sender_address => %{"Assemblies IT Support" <assembly-it@cornell.edu>},
