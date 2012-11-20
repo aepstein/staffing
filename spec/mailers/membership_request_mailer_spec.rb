3d2bf0044f476260a807c6eea872f2a273b9ea2f
Ari Epstein <aepstein607@gmail.com>
2012-11-20 14:07:12 -0500

    Move request mailer cucumber to spec.

--- spec/mailers/membership_request_mailer_spec.rb
+++ spec/mailers/membership_request_mailer_spec.rb
@@ -3,14 +3,15 @@ require "spec_helper"
 describe MembershipRequestMailer do
   include MailerSpecHelpers
   let(:position) { create( :position,
+    name: 'Interesting Position',
     authority: create(:authority,
       contact_name: "James Madison",
       contact_email: "madison@example.com",
       reject_message: "Alternatives for the *authority*.") ) }
-  let(:committee) { create :committee,
+  let(:committee) { create :committee, name: 'Important Committee',
     reject_message: "Alternatives to the *committee*." }
-  let(:enrollment) { create :enrollment, requestable: true,
-    committee: committee, position: position }
+  let(:enrollment) { create :enrollment, requestable: true, votes: 1,
+    committee: committee, position: position, title: 'member' }
   let(:membership_request) { create(:membership_request, committee: enrollment.committee) }
 
   def should_be_to_requestor
@@ -44,6 +45,44 @@ EOS
       html_part_should_match /Alternatives to the <em>committee<\/em>\./
       text_part_should_match /Alternatives for the \*authority\*\./
       html_part_should_match /Alternatives for the <em>authority<\/em>\./
+    end
+  end
+
+  describe "close" do
+    before(:each) do
+      membership_request.close!
+    end
+    let(:mail) { MembershipRequestMailer.close_notice( membership_request ) }
+
+    it "has correct headers" do
+      mail.subject.should eq "Your request for appointment to #{membership_request.committee} was approved"
+      should_be_to_requestor
+      mail.from.should eq(["info@example.org"])
+    end
+
+    it "renders the correct messages in the body with no memberships" do
+      both_parts_should_match /Dear #{membership_request.user.first_name},/
+      both_parts_should_match <<EOS.gsub(/\s+/, " ").strip
+This notice is to inform you that your request for appointment to
+#{membership_request.committee} has been approved.
+EOS
+      both_parts_should_match /You have not been appointed to any positions as a result of this request./
+    end
+
+    it "renders membership information in the body with enrolled memberships" do
+      membership = create(:membership,
+        user: membership_request.user, position: enrollment.position)
+      membership_request.memberships << membership
+      both_parts_should_match /You have been appointed to the following position:/
+      both_parts_should_match /member in Important Committee for a term beginning #{membership.starts_at.to_formatted_s :us_ordinal} and ending #{membership.ends_at.to_formatted_s :us_ordinal}/
+    end
+
+    it "renders membership information in the body with unenrolled memberships" do
+      membership = create(:membership, user: membership_request.user,
+        position: create(:position, name: 'Insignificant Position'))
+      membership_request.memberships << membership
+      both_parts_should_match /You have been appointed to the following position:/
+      both_parts_should_match /Insignificant Position for a term beginning #{membership.starts_at.to_formatted_s :us_ordinal} and ending #{membership.ends_at.to_formatted_s :us_ordinal}/
     end
   end
 end
