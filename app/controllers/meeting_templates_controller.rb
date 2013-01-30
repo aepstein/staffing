257a1dbfd2972c584b1e8c52fb373c52945811a8
Ari Epstein <aepstein607@gmail.com>
2013-01-30 15:08:23 -0500

    Prepare meeting templates and meetings controllers and views.

--- app/controllers/meeting_templates_controller.rb
+++ app/controllers/meeting_templates_controller.rb
@@ -1,55 +1,28 @@
 class MeetingTemplatesController < ApplicationController
-  before_filter :initialize_context
-  before_filter :new_meeting_template_from_params, only: [ :new, :create ]
+  expose :q_scope { MeetingTemplate.scoped }
+  expose :search { params[:term] ? { name_cont: params[:term] } : params[:q] }
+  expose :q { q_scope.search( search ) }
+  expose :meeting_templates { q.result.ordered.page(params[:page]) }
+  expose :meeting_template
   filter_resource_access
-  before_filter :setup_breadcrumbs
 
   # GET /meeting_templates
-  # GET /meeting_templates.xml
   def index
-    search = params[:term] ? { name_cont: params[:term] } : params[:q]
-    @q ||= MeetingTemplate.search( search )
-    @meeting_templates = @q.result.ordered.page( params[:page] )
-
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @meeting_templates }
     end
-  end
-
-  # GET /meeting_templates/1
-  # GET /meeting_templates/1.xml
-  def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @meeting_template }
-    end
-  end
-
-  # GET /meeting_templates/new
-  # GET /meeting_templates/new.xml
-  def new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @meeting_template }
-    end
-  end
-
-  # GET /meeting_templates/1/edit
-  def edit
   end
 
   # POST /meeting_templates
   # POST /meeting_templates.xml
   def create
     respond_to do |format|
-      if @meeting_template.save
-        flash[:notice] = 'Meeting template was successfully created.'
-        format.html { redirect_to(@meeting_template) }
-        format.xml  { render xml: @meeting_template, status: :created, location: @meeting_template }
+      if meeting_template.save
+        format.html { redirect_to( meeting_template, flash: { success: 'Meeting template created.' } ) }
+        format.xml  { render xml: meeting_template, status: :created, location: meeting_template }
       else
         format.html { render action: "new" }
-        format.xml  { render xml: @meeting_template.errors, status: :unprocessable_entity }
+        format.xml  { render xml: meeting_template.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -58,13 +31,12 @@ class MeetingTemplatesController < ApplicationController
   # PUT /meeting_templates/1.xml
   def update
     respond_to do |format|
-      if @meeting_template.update_attributes(params[:meeting_template])
-        flash[:notice] = 'Meeting template was successfully updated.'
-        format.html { redirect_to(@meeting_template) }
+      if meeting_template.save
+        format.html { redirect_to( meeting_template, flash: { success: "Meeting template updated." } ) }
         format.xml  { head :ok }
       else
         format.html { render action: "edit" }
-        format.xml  { render xml: @meeting_template.errors, status: :unprocessable_entity }
+        format.xml  { render xml: meeting_template.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -72,28 +44,11 @@ class MeetingTemplatesController < ApplicationController
   # DELETE /meeting_templates/1
   # DELETE /meeting_templates/1.xml
   def destroy
-    @meeting_template.destroy
+    meeting_template.destroy
 
     respond_to do |format|
-      format.html { redirect_to(meeting_templates_url, notice: "Meeting template was successfully destroyed.") }
+      format.html { redirect_to(meeting_templates_url, flash: { sucess: "Meeting template destroyed." } ) }
       format.xml  { head :ok }
-    end
-  end
-
-  private
-
-  def initialize_context
-    @meeting_template = MeetingTemplate.find params[:id] if params[:id]
-  end
-
-  def new_meeting_template_from_params
-    @meeting_template = MeetingTemplate.new( params[:meeting_template] )
-  end
-
-  def setup_breadcrumbs
-    add_breadcrumb 'Meeting templates', meeting_templates_path
-    if @meeting_template && @meeting_template.persisted?
-      add_breadcrumb @meeting_template, meeting_template_path( @meeting_template )
     end
   end
 end
--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,24 +1,33 @@
 class MeetingsController < ApplicationController
-  before_filter :initialize_context
-  before_filter :new_meeting_from_params, only: [ :new, :create ]
-  before_filter :populate_meeting_sections, only: [ :new, :edit ]
-  before_filter :initialize_index, only: [ :current, :past, :future, :index ]
-  before_filter :setup_breadcrumbs
-  filter_access_to :new, :create, :edit, :update, :destroy, :show, :publish,
-    attribute_check: true
-  filter_access_to :index, :current, :past, :future do
-    true
+  expose :committee { Committee.find params[:committee_id] if params[:committee_id] }
+  expose :motion { Motion.find params[:motion_id] if params[:motion_id] }
+  expose :q_scope do
+    scope ||= committee.meetings if committee
+    scope ||= motion.meetings if motion
+    scope ||= Meeting.scoped
+    scope = scope.past if request.action_name == 'past'
+    scope = scope.current if request.action_name == 'current'
+    scope = scope.future if request.action_name == 'future'
   end
+  expose :q { q_scope.search }
+  expose :meetings do
+    scope = q.result.with_permissions_to(:show).ordered.page(params[:page])
+  end
+  expose :meeting
+  before_filter :populate_meeting_sections, only: [ :new, :edit ]
+  filter_access_to :new, :create, :edit, :update, :destroy, :show, :publish,
+    attribute_check: true, load_method: :meeting
+  filter_access_to :index, :current, :past, :future
   filter_access_to :editable_minutes, :published_minutes, :audio, :agenda do
-    permitted_to! :show
+    permitted_to! :show, meeting
   end
 
   # GET /meetings/:id/agenda.pdf
   def agenda
     respond_to do |format|
       format.pdf do
-        report = MeetingAgendaReport.new( @meeting )
-        send_data report.to_pdf, filename: "#{@meeting.to_s :file}-agenda.pdf",
+        report = MeetingAgendaReport.new( meeting )
+        send_data report.to_pdf, filename: "#{meeting.to_s :file}-agenda.pdf",
           type: 'application/pdf', disposition: 'inline'
       end
     end
@@ -29,17 +38,17 @@ class MeetingsController < ApplicationController
   def publish
     respond_to do |format|
       if request.method_symbol == :get
-        @meeting.publish_defaults
+        meeting.publish_defaults
         format.html { render action: :publish }
       else
-        @meeting.assign_attributes params[:meeting], as: :publisher
-        @meeting.publish_from = current_user.email
-        if @meeting.publish
-          format.html { redirect_to(@meeting, notice: 'Meeting was successfully published.') }
+        meeting.assign_attributes params[:meeting], as: :publisher
+        meeting.publish_from = current_user.email
+        if meeting.publish
+          format.html { redirect_to( meeting, flash: { success: 'Meeting published.' } ) }
           format.xml  { head :ok }
         else
-          format.html { render action: :publish, alert: 'You must specify a recipient.' }
-          format.xml  { render xml: @meeting.errors, status: :unprocessable_entity }
+          format.html { render action: :publish, flash: { error: 'You must specify a recipient.' } }
+          format.xml  { render xml: meeting.errors, status: :unprocessable_entity }
         end
       end
     end
@@ -47,10 +56,10 @@ class MeetingsController < ApplicationController
 
   # GET /meetings/:id/editable_minutes.(doc|odt|tex|txt)
   def editable_minutes
-    send_file @meeting.editable_minutes.current_path,
-      :filename => @meeting.to_s(:editable_minutes_file),
-      :content_type => @meeting.editable_minutes.content_type,
-      :disposition => 'attachment'
+    send_file meeting.editable_minutes.current_path,
+      filename: meeting.to_s(:editable_minutes_file),
+      content_type: meeting.editable_minutes.content_type,
+      disposition: 'attachment'
   end
 
   # GET /meetings/current
@@ -60,8 +69,6 @@ class MeetingsController < ApplicationController
   # GET /motions/:motion_id/meetings/current
   # GET /motions/:motion_id/meetings/current.xml
   def current
-    @meetings = @meetings.current
-    add_breadcrumb 'Current', polymorphic_path([ :current, @context, :meetings ])
     index
   end
 
@@ -72,8 +79,6 @@ class MeetingsController < ApplicationController
   # GET /motions/:motion_id/meetings/past
   # GET /motions/:motion_id/meetings/past.xml
   def past
-    @meetings = @meetings.past
-    add_breadcrumb 'Past', polymorphic_path([ :past, @context, :meetings ])
     index
   end
 
@@ -84,8 +89,6 @@ class MeetingsController < ApplicationController
   # GET /motions/:motion_id/meetings/future
   # GET /motions/:motion_id/meetings/future.xml
   def future
-    @meetings = @meetings.future
-    add_breadcrumb 'Future', polymorphic_path([ :future, @context, :meetings ])
     index
   end
 
@@ -94,52 +97,27 @@ class MeetingsController < ApplicationController
   # GET /motions/:motion_id/meetings
   # GET /motions/:motion_id/meetings.xml
   def index
-    @q = @meetings.search( params[:q] )
-    @meetings = @q.result.page( params[:page] )
-
     respond_to do |format|
       format.html { render action: 'index' } # index.html.erb
-      format.xml  { render xml: @meetings }
+      format.xml  { render xml: meetings }
     end
-  end
-
-  # GET /meetings/1
-  # GET /meetings/1.xml
-  def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render xml: @meeting }
-    end
-  end
-
-  # GET /committees/:committee_id/meetings/new
-  # GET /committees/:committee_id/meetings/new.xml
-  def new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render xml: @meeting }
-    end
-  end
-
-  # GET /meetings/1/edit
-  def edit
   end
 
   # POST /committees/:committee_id/meetings
   # POST /committees/:committee_id/meetings.xml
   def create
-    @meeting.meeting_sections.each do |section|
+    meeting.meeting_sections.each do |section|
       section.meeting_items.each do |item|
         item.attachments.each { |attachment| attachment.attachable = item }
       end
     end
     respond_to do |format|
-      if @meeting.save
-        format.html { redirect_to( @meeting, notice: 'Meeting was successfully created.' ) }
-        format.xml  { render xml: @meeting, status: :created, location: @meeting }
+      if meeting.save
+        format.html { redirect_to( meeting, flash: { success: 'Meeting created.' } ) }
+        format.xml  { render xml: meeting, status: :created, location: meeting }
       else
         format.html { render action: "new" }
-        format.xml  { render xml: @meeting.errors, status: :unprocessable_entity }
+        format.xml  { render xml: meeting.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -148,12 +126,12 @@ class MeetingsController < ApplicationController
   # PUT /meetings/1.xml
   def update
     respond_to do |format|
-      if @meeting.update_attributes(params[:meeting])
-        format.html { redirect_to @meeting, notice: 'Meeting was successfully updated.' }
+      if meeting.save
+        format.html { redirect_to meeting, flash: { success: 'Meeting updated.' } }
         format.xml  { head :ok }
       else
         format.html { render action: "edit" }
-        format.xml  { render xml: @meeting.errors, status: :unprocessable_entity }
+        format.xml  { render xml: meeting.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -161,59 +139,19 @@ class MeetingsController < ApplicationController
   # DELETE /meetings/1
   # DELETE /meetings/1.xml
   def destroy
-    @meeting.destroy
+    meeting.destroy
 
     respond_to do |format|
-      format.html { redirect_to( committee_meetings_url( @meeting.committee ),
-        notice: "Meeting was successfully destroyed." ) }
+      format.html { redirect_to( committee_meetings_url( meeting.committee ),
+        flash: { success: "Meeting destroyed." } ) }
       format.xml  { head :ok }
     end
   end
 
   private
 
-  def new_meeting_from_params
-    @meeting = @committee.meetings.build( params[:meeting] )
-    @meeting.period ||= @committee.periods.active
-  end
-
-  def initialize_index
-    @meetings = @committee.meetings if @committee
-    @meetings = @motion.meetings if @motion
-    @meetings ||= Meeting.scoped
-    @meetings = @meetings.with_permissions_to(:show)
-  end
-
-  def initialize_context
-    @meeting = Meeting.find(params[:id]) if params[:id]
-    @committee = Committee.find(params[:committee_id]) if params[:committee_id]
-    @motion = Motion.find(params[:motion_id]) if params[:motion_id]
-    @committee ||= @meeting.committee if @meeting
-    @committee ||= @motion.committee if @motion
-    @context = @motion || @committee
-  end
-
   def populate_meeting_sections
-    @meeting.meeting_sections.populate
-  end
-
-  def setup_breadcrumbs
-    add_breadcrumb 'Committees', committees_path
-    if @committee
-      add_breadcrumb @committee.name, committee_path(@committee)
-    end
-    if @motion
-      add_breadcrumb 'Motions', committee_motions_path(@committee)
-      add_breadcrumb @motion.tense.to_s.capitalize,
-        polymorphic_path([ @motion.tense, @committee, :motions ])
-      add_breadcrumb @motion.name, motion_path(@motion)
-    end
-    add_breadcrumb 'Meetings', polymorphic_path([ @context, :meetings ])
-    if @meeting && @meeting.persisted?
-      add_breadcrumb @meeting.tense.to_s.capitalize,
-        polymorphic_path([ @meeting.tense, @context, :meetings ])
-      add_breadcrumb @meeting, meeting_path( @meeting )
-    end
+    meeting.meeting_sections.populate
   end
 end
 
