b951f3ec604a88550da7cf870e3dc20e1b8d7b04
Ari Epstein <aepstein607@gmail.com>
2013-01-30 15:40:52 -0500

    Membership requests updated for exposures and simple_form+bootstrap.

--- app/controllers/membership_requests_controller.rb
+++ app/controllers/membership_requests_controller.rb
@@ -1,16 +1,50 @@
 class MembershipRequestsController < ApplicationController
   before_filter :require_user
-  before_filter :initialize_context
-  before_filter :initialize_index, :only => [ :index, :expired, :unexpired,
-    :active, :inactive, :rejected ]
-  before_filter :new_membership_request_from_params, :only => [ :new, :create ]
+  before_filter :populate_answers, only: [ :new, :edit ]
+  expose :committee { Committee.find params[:committee_id] if params[:committee_id] }
+  expose :authority { Authority.find params[:authority_id] if params[:authority_id] }
+  expose :user { User.find params[:user_id] if params[:user_id] }
+  expose :context { authority || committee || user }
+  expose :q_scope do
+    scope = context.membership_requests.scoped if context
+    scope ||= MembershipRequest.scoped
+    scope = scope.expired if request.action_name == 'expired'
+    scope = scope.unexpired if request.action_name == 'unexpired'
+    scope = scope.active if request.action_name == 'active'
+    scope = scope.inactive if request.action_name == 'inactive'
+    scope = scope.rejected if request.action_name == 'rejected'
+  end
+  expose :q do
+    q_scope.search( params[:q] )
+  end
+  expose :membership_requests do
+    q.result.ordered.with_permissions_to(:show)
+  end
+  expose :assigner_role do
+    case action_name
+    when 'reject'
+      :rejector
+    else
+      :default
+    end
+  end
+  expose :membership_request do
+    out = if params[:id]
+      MembershipRequest.find params[:id]
+    else
+      committee.membership_requests.where( user_id: current_user.id ).first ||
+      committee.membership_requests.build
+    end
+    out.user ||= current_user
+    out.assign_attributes params[:membership_request], as: assigner_role
+    out
+  end
   filter_access_to :new, :create, :edit, :update, :destroy, :show, :reject,
-    :do_reject, :reactivate, attribute_check: true
+    :reactivate, attribute_check: true, load_method: :membership_request
   filter_access_to :index, :renewed, :unrenewed, :expired, :unexpired, :active,
     :inactive, :rejected do
-    @user ? permitted_to!( :show, @user ) : permitted_to!( :index )
+    user ? permitted_to!( :show, user ) : permitted_to!( :index )
   end
-  before_filter :setup_breadcrumbs
 
   # GET /position/:position_id/membership_requests
   # GET /position/:position_id/membership_requests.xml
@@ -19,91 +53,44 @@ class MembershipRequestsController < ApplicationController
   # GET /user/:user_id/membership_requests
   # GET /user/:user_id/membership_requests.xml
   def index
-    @q = @membership_requests.search( params[:q] )
-    @membership_requests = @q.result.page( params[:page] )
-
     unless params[:format] == 'csv'
-      @membership_requests = @membership_requests.page( params[:page] )
+      membership_requests = membership_requests.page( params[:page] )
     end
 
     respond_to do |format|
       format.csv { index_csv }
-      format.html { render :action => 'index' } # index.html.erb
-      format.xml  { render :xml => @membership_requests }
+      format.html { render action: 'index' } # index.html.erb
     end
   end
 
   # GET /user/:user_id/membership_requests/active
   # GET /user/:user_id/membership_requests/active.xml
   def active
-    @membership_requests = @membership_requests.active
-    add_breadcrumb 'Active', polymorphic_path( [ :active, @context, :membership_requests ] )
     index
   end
 
   # GET /user/:user_id/membership_requests/inactive
   # GET /user/:user_id/membership_requests/inactive.xml
   def inactive
-    @membership_requests = @membership_requests.inactive
-    add_breadcrumb 'Inactive', polymorphic_path( [ :inactive, @context, :membership_requests ] )
     index
   end
 
   # GET /user/:user_id/membership_requests/rejected
   # GET /user/:user_id/membership_requests/rejected.xml
   def rejected
-    @membership_requests = @membership_requests.rejected
-    add_breadcrumb 'Rejected', polymorphic_path( [ :rejected, @context, :membership_requests ] )
     index
   end
 
   # GET /user/:user_id/membership_requests/expired
   # GET /user/:user_id/membership_requests/expired.xml
   def expired
-    @membership_requests = @membership_requests.expired
-    add_breadcrumb 'Expired', polymorphic_path( [ :expired, @context, :membership_requests ] )
     index
   end
 
   # GET /user/:user_id/membership_requests/unexpired
   # GET /user/:user_id/membership_requests/unexpired.xml
   def unexpired
-    @membership_requests = @membership_requests.unexpired
-    add_breadcrumb 'Unexpired', polymorphic_path( [ :unexpired, @context, :membership_requests ] )
     index
-  end
-
-  # GET /membership_requests/1
-  # GET /membership_requests/1.xml
-  def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @membership_request }
-    end
-  end
-
-  # GET /positions/:position_id/membership_requests/new
-  # GET /positions/:position_id/membership_requests/new.xml
-  # GET /committees/:committee_id/membership_requests/new
-  # GET /committees/:committee_id/membership_requests/new.xml
-  # GET /memberships/:membership_id/membership_requests/new
-  # GET /memberships/:membership_id/membership_requests/new.xml
-  def new
-    @membership_request.answers.populate
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @membership_request }
-    end
-  end
-
-  # GET /membership_requests/1/edit
-  def edit
-    @membership_request.answers.populate
-
-    respond_to do |format|
-      format.html { render :action => 'edit' }
-    end
   end
 
   # POST /positions/:position_id/membership_requests
@@ -114,13 +101,10 @@ class MembershipRequestsController < ApplicationController
   # POST /memberships/:membership_id/membership_requests.xml
   def create
     respond_to do |format|
-      if @membership_request.save
-        flash[:notice] = 'Membership request was successfully created.'
-        format.html { redirect_to(@membership_request) }
-        format.xml  { render :xml => @membership_request, :status => :created, :location => @membership_request }
+      if membership_request.save
+        format.html { redirect_to( membership_request, flash: { success: "Membership request created." } ) }
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @membership_request.errors, :status => :unprocessable_entity }
+        format.html { render action: "new" }
       end
     end
   end
@@ -129,14 +113,10 @@ class MembershipRequestsController < ApplicationController
   # PUT /membership_requests/1.xml
   def update
     respond_to do |format|
-      @membership_request.assign_attributes(params[:membership_request])
-      if ( @membership_request.active? && @membership_request.save ) || @membership_request.reactivate
-        flash[:notice] = 'Membership request was successfully updated.'
-        format.html { redirect_to @membership_request }
-        format.xml  { head :ok }
+      if ( membership_request.active? && membership_request.save ) || membership_request.reactivate
+        format.html { redirect_to( membership_request, flash: { success: "Membership request updated and active." } ) }
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @membership_request.errors, :status => :unprocessable_entity }
+        format.html { render action: "edit" }
       end
     end
   end
@@ -146,39 +126,26 @@ class MembershipRequestsController < ApplicationController
   def reject
     respond_to do |format|
       if request.request_method_symbol == :put
-        @membership_request.rejected_by_user = current_user
-        @membership_request.assign_attributes params[:membership_request], as: :rejector
-        if @membership_request.reject
-          flash[:notice] = 'Membership request was successfully rejected.'
-          format.html { redirect_to @membership_request }
-          format.xml  { head :ok }
+        membership_request.rejected_by_user = current_user
+        if membership_request.reject
+          format.html { redirect_to( membership_request, flash: { success: 'Membership request rejected.' } ) }
         else
-          format.html { render :action => "reject" }
-          format.xml  { render :xml => @membership_request.errors, :status => :unprocessable_entity }
+          format.html { render action: "reject" }
         end
       else
-        @membership_request.rejected_by_authority ||= ( current_user.authorities.authorized.to_a & @membership_request.authorities ).first
-        format.html
+        membership_request.rejected_by_authority ||= ( current_user.authorities.authorized.to_a & membership_request.authorities ).first
       end
     end
-  end
-
-  # PUT /membership_requests/1/do_reject
-  # PUT /membership_requests/1/do_reject.xml
-  def do_reject
   end
 
   # PUT /membership_requests/1/reactivate
   # PUT /membership_requests/1/reactivate.xml
   def reactivate
     respond_to do |format|
-      if @membership_request.reactivate
-        flash[:notice] = 'Membership request was successfully reactivated.'
-        format.html { redirect_to @membership_request }
-        format.xml  { head :ok }
+      if membership_request.reactivate
+        format.html { redirect_to( membership_request, flash: { success: "Membership request reactivated." } ) }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @membership_request.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -186,67 +153,32 @@ class MembershipRequestsController < ApplicationController
   # DELETE /membership_requests/1
   # DELETE /membership_requests/1.xml
   def destroy
-    @membership_request.destroy
+    membership_request.destroy
 
     respond_to do |format|
-      format.html { redirect_to polymorphic_url( [ @membership_request.committee, :membership_requests ] ),
-        notice: 'Membership request was successfully destroyed.' }
+      format.html { redirect_to( polymorphic_url( [ membership_request.committee, :membership_requests ] ),
+        flash: { success: 'Membership request destroyed.' } ) }
       format.xml  { head :ok }
     end
   end
 
   private
 
-  def initialize_context
-    @membership_request = MembershipRequest.find( params[:id] ) if params[:id]
-    @committee = @membership_request.committee if @membership_request
-    @committee = Committee.find params[:committee_id] if params[:committee_id]
-    @authority = Authority.find( params[:authority_id] ) if params[:authority_id]
-    @user = User.find(params[:user_id]) if params[:user_id]
-    if @committee
-      @membership_request ||= @committee.membership_requests.where( user_id: current_user.id ).first
-    end
-    @context = @authority || @committee || @user
-  end
-
-  def initialize_index
-    @membership_requests = @context.membership_requests.ordered.with_permissions_to( :show )
-    @title = "for #{@context}"
-  end
-
-  def new_membership_request_from_params
-    if @membership_request && @membership_request.persisted?
-      return redirect_to edit_membership_request_url( @membership_request )
-    end
-    @membership_request = @committee.membership_requests.build
-    @membership_request.starts_at ||= @membership.starts_at if @membership
-    @membership_request.user ||= current_user
-    @membership_request.assign_attributes params[:membership_request]
-  end
-
-  def setup_breadcrumbs
-    if @context
-      add_breadcrumb @context.class.to_s.pluralize,
-        polymorphic_path( [ @context.class.arel_table.name ] )
-      add_breadcrumb @context, polymorphic_path( [ @context ] )
-    end
-    add_breadcrumb "MembershipRequests", polymorphic_path( [ @context, :membership_requests ] )
-    if @membership_request && @membership_request.persisted?
-      add_breadcrumb @membership_request, membership_request_path( @membership_request )
-    end
-  end
-
   def index_csv
     csv_string = CSV.generate do |csv|
       csv << %w( net_id first last status committee until )
-      @membership_requests.all.each do |membership_request|
+      membership_requests.all.each do |membership_request|
         csv << [ membership_request.user.net_id, membership_request.user.first_name,
           membership_request.user.last_name, membership_request.user.status,
           membership_request.committee, membership_request.ends_at ]
       end
     end
-    send_data csv_string, :disposition => "attachment; filename=membership_requests.csv",
-      :type => :csv
+    send_data csv_string, disposition: "attachment; filename=membership_requests.csv",
+      type: :csv
+  end
+
+  def populate_answers
+    membership_request.answers.populate
   end
 
 end
