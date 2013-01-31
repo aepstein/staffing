c36e29cd99498cab074cb71c527879522589340d
Ari Epstein <aepstein607@gmail.com>
2013-01-31 10:10:16 -0500

    Factor out time formats into ui gem.

--- config/initializers/time_formats.rb
+++
@@ -1,4 +0,0 @@
-Time::DATE_FORMATS[:us_ordinal] = lambda { |time| time.strftime "%B #{ActiveSupport::Inflector.ordinalize time.day}, %Y %I:%M%P " }
-Time::DATE_FORMATS[:short_year] = "%d %b %Y %H:%M"
-Time::DATE_FORMATS[:us_time] = "%l:%M%P"
-
