8bb8b06e669f6a55175b8f7ba17d25b2d32bf802
Ari Epstein <aepstein607@gmail.com>
2013-02-10 16:37:25 -0500

    Not necessary to set compressor anymore.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,6 @@ Staffing::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
   config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
