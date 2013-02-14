505dd88ed6f345d42c1a5e369cb5fe169eafa98e
Ari Epstein <aepstein607@gmail.com>
2013-02-14 10:44:00 -0500

    Fix mail part testers to work with multipart messages that have attachments.

--- spec/support/mailer_spec_helpers.rb
+++ spec/support/mailer_spec_helpers.rb
@@ -1,10 +1,22 @@
 module MailerSpecHelpers
+  def mail_core
+    @mail_core ||= mail.parts.select { |p| p.content_type =~ /^multipart\/alternative/ }.first || mail
+  end
+
+  def text_part
+    @text_part ||= mail_core.parts.select { |p| p.content_type =~ /^text\/plain/ }.first
+  end
+
+  def html_part
+    @html_part ||= mail_core.parts.select { |p| p.content_type =~ /^text\/html/ }.first
+  end
+
   def text_part_should_not_match( content )
-    mail.parts[0].body.encoded.should_not match(content)
+    text_part.body.encoded.should_not match(content)
   end
 
   def html_part_should_not_match( content )
-    mail.parts[1].body.encoded.should_not match(content)
+    html_part.body.encoded.should_not match(content)
   end
 
   def both_parts_should_not_match( content )
@@ -13,11 +25,11 @@ module MailerSpecHelpers
   end
 
   def text_part_should_match( content )
-    mail.parts[0].body.encoded.should match(content)
+    text_part.body.encoded.should match(content)
   end
 
   def html_part_should_match( content )
-    mail.parts[1].body.encoded.should match(content)
+    html_part.body.encoded.should match(content)
   end
 
   def both_parts_should_match( content )
