5fa279d0767b518ebeba9eb9c912d9c05a7f5d62
Ari Epstein <aepstein607@gmail.com>
2013-04-26 11:44:51 -0400

    Fix motion comment attachment issue.

--- app/models/motion_comment.rb
+++ app/models/motion_comment.rb
@@ -8,5 +8,14 @@ class MotionComment < ActiveRecord::Base
   accepts_nested_attributes_for :attachments, allow_destroy: true
 
   has_paper_trail
+
+  def to_s(format=nil)
+    case format
+    when :file
+      "#{motion.to_s(:file)}-comment-#{id}"
+    else
+      super()
+    end
+  end
 end
 
