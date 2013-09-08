7c6dfea1c34d9fc07d3ed2de6dc3e296df8d0be0
Ari Epstein <aepstein607@gmail.com>
2013-09-07 21:11:09 -0400

    Update exception notification code in preparation for deployment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,9 +12,10 @@ Staffing::Application.configure do
   config.assets.digest = true
   config.filter_parameters += [ :password, :password_confirmation ]
   config.assets.precompile += %w( ie6.css ie7.css print.css )
-  config.middleware.use ExceptionNotifier,
-    :email_prefix => "[staffing] ",
-    :sender_address => %{"Assemblies IT Support" <assembly-it@cornell.edu>},
-    :exception_recipients => %w{assembly-it@cornell.edu}
+  config.middleware.use ExceptionNotification::Rack, email: {
+    email_prefix: "[staffing] ",
+    sender_address: %{"Assemblies IT Support" <assembly-it@cornell.edu>},
+    exception_recipients: %w{assembly-it@cornell.edu}
+  }
 end
 
