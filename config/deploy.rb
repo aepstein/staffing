d6109593b9fb138d0fdb0baae8b04ed0b5180508
Ari Epstein <aepstein607@gmail.com>
2014-03-07 15:46:09 -0500

    Temporary files directory should be shared between installations.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, 'staffing'
 set :repo_url, "git://assembly.cornell.edu/git/#{fetch(:application)}.git"
 set :deploy_to, "/var/www/assembly/#{fetch(:application)}"
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{db/uploads public/system db/fonts log}
+set :linked_dirs, %w{db/uploads public/system db/fonts log tmp}
 set :default_env, {
   "RAILS_RELATIVE_URL_ROOT" => "/staffing"
 }
