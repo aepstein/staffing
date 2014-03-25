75dd3f36c18595e8a64141c36baf45051dea0746
Ari Epstein <aepstein607@gmail.com>
2014-03-25 07:31:39 -0400

    Fix typo in last commit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Staffing::Application.configure do
   config.assets.digest = true
   config.filter_parameters += [ :password, :password_confirmation ]
   config.assets.precompile += %w( ie6.css ie7.css print.css )
-  config.threadsafe! unless ENV['THREADSAFE'] = 'off'
+  config.threadsafe! unless ENV['THREADSAFE'] == 'off'
   config.middleware.use ExceptionNotification::Rack, email: {
     email_prefix: "[staffing] ",
     sender_address: %{"Assemblies IT Support" <assembly-it@cornell.edu>},
