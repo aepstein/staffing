378982b0a1b512c9b8838c0c2adec7e241d6aa3e
Ari Epstein <aepstein607@gmail.com>
2014-08-12 11:39:20 -0400

    Set up new secret token settings.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Staffing::Application.config.secret_token = Staffing::Application.app_config['session_secret']
+Staffing::Application.config.secret_key_base = Staffing::Application.app_config['session_secret_key_base']
 
